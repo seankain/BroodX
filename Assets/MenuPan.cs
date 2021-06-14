1cebabc1674f6c37b93c94164476b85e88eb11aa
unknown <sean.kain.geo@gmail.com>
2021-06-13 22:43:50 -0400

    Ragdolling on death, menu pan

---
+++ Assets/MenuPan.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPan : MonoBehaviour
+{
+    [SerializeField]
+    private string CameraPanTag = "CameraPan";
+    [SerializeField]
+    private float Speed = 0.5f;
+    private Vector3 CenterFocus = Vector3.zero;
+    private Camera cam;
+    private GameObject[] panLocations;
+    private int currentIndex = 0;
+    private Vector3 currentTarget = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        panLocations = GameObject.FindGameObjectsWithTag(CameraPanTag);
+        cam.transform.position = panLocations[0].transform.position;
+        cam.transform.LookAt(CenterFocus);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.transform.LookAt(CenterFocus);
+        cam.transform.position = Vector3.MoveTowards(cam.transform.position,currentTarget,Time.deltaTime * Speed);
+        if(Vector3.Distance(cam.transform.position,currentTarget) < 1)
+        {
+            SetLocation();
+        }
+    }
+
+    private void OnEnable()
+    {
+        cam.transform.position = panLocations[0].transform.position;
+        cam.transform.LookAt(CenterFocus);
+    }
+
+    private void SetLocation() 
+    {
+        currentTarget = panLocations[currentIndex].transform.position;
+        currentIndex++;
+        if (currentIndex > panLocations.Length) { currentIndex = 0; }
+    }
+
+}
--- Assets/Scripts/BroodPlayer.cs
+++ Assets/Scripts/BroodPlayer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Invector.CharacterController;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,26 +11,56 @@ public class BroodPlayer : MonoBehaviour
     public bool Grounded { get; private set; }
     public bool Running { get; private set; }
     public bool Stationary { get; private set; }
+
+    [SerializeField]
+    private float minCameraDistance = 2;
+    [SerializeField]
+    private float maxCameraDistance = 10;
+    [SerializeField]
+    private AudioSource CicadaHitSound;
+    [SerializeField]
+    private FootfallEmitter footFallEmitter;
+
+    public Camera PlayerCamera;
+    private vThirdPersonCamera tpc;
+    private vThirdPersonController invectorController;
+    private vThirdPersonInput invectorInput;
     public Vector3 Velocity { get; private set; }
     private Vector3 respawnLocation = Vector3.zero;
     private float RespawnTime = 3;
     private float DeadElapsed = 0;
-    
+    private CapsuleCollider playerCollider;
+    private Animator anim;
 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerCollider = GetComponent<CapsuleCollider>();
+        invectorController = GetComponent<vThirdPersonController>();
+        invectorInput = GetComponent<vThirdPersonInput>();
+        anim = GetComponent<Animator>();
+        SetRagdoll(false);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
         respawnLocation = gameObject.transform.position;
+        tpc = PlayerCamera.GetComponent<vThirdPersonCamera>();
+        footFallEmitter = GetComponentInChildren<FootfallEmitter>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        var scroll = Input.GetAxis("Mouse ScrollWheel");
+        tpc.defaultDistance += -scroll;
+        if (tpc.defaultDistance > maxCameraDistance) { tpc.defaultDistance = maxCameraDistance; }
+        if (tpc.defaultDistance < minCameraDistance) { tpc.defaultDistance = minCameraDistance; }
+        if (Input.GetKeyDown(KeyCode.R)) { Die(); }
         if (Dead)
         {
             DeadElapsed += Time.deltaTime;
-            if(DeadElapsed >= RespawnTime)
+            if (DeadElapsed >= RespawnTime)
             {
                 DeadElapsed = 0;
                 Respawn();
@@ -40,6 +71,23 @@ public class BroodPlayer : MonoBehaviour
         Velocity = rb.velocity;
     }
 
+    private void SetRagdoll(bool ragdollActivated)
+    {
+        var colliders = GetComponentsInChildren<Collider>();
+        playerCollider.enabled = !ragdollActivated;
+        anim.enabled = !ragdollActivated;
+        rb.velocity = Vector3.zero;
+        rb.isKinematic = ragdollActivated;
+        foreach (var collider in colliders)
+        {
+            if (collider != playerCollider)
+            {
+                collider.isTrigger = !ragdollActivated;
+                collider.attachedRigidbody.isKinematic =!ragdollActivated;
+                collider.attachedRigidbody.velocity = Vector3.zero;
+            }
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         var cicada = collision.collider.gameObject.GetComponent<Cicada>();
@@ -58,7 +106,7 @@ public class BroodPlayer : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name == "Goal")
+        if (other.gameObject.name == "Goal")
         {
             other.gameObject.GetComponent<AudioSource>().Play();
         }
@@ -66,8 +114,13 @@ public class BroodPlayer : MonoBehaviour
 
     void Die()
     {
+        CicadaHitSound.Play();
+        footFallEmitter.enabled = false;
         rb.constraints = (RigidbodyConstraints.None);
         Dead = true;
+        invectorController.enabled = false;
+        invectorInput.enabled = false;
+        SetRagdoll(true);
     }
 
     void Respawn()
@@ -75,7 +128,12 @@ public class BroodPlayer : MonoBehaviour
         this.gameObject.transform.rotation = Quaternion.identity;
         this.gameObject.transform.position = respawnLocation;
         rb.constraints = RigidbodyConstraints.FreezeRotation;
+        footFallEmitter.enabled = true;
         Dead = false;
+        invectorController.enabled = true;
+        invectorInput.enabled = true;
+        SetRagdoll(false);
+        
     }
 
 }
